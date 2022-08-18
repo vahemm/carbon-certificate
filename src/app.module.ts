6a25bc9f22655b97e1b8bf34babfd39b21a06d54
Vahe Manukyan <vahemm@outlook.com>
2022-08-18 13:01:51 +0400

    Add users and authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database/database.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    AuthenticationModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/authentication/authentication.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { UsersModule } from '../users/users.module';
+import { AuthenticationController } from './authentication.controller';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import User from 'src/users/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User]),
+    UsersModule,
+    PassportModule,
+    ConfigModule,
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => ({
+      secret: configService.get('JWT_SECRET'),
+      signOptions: {
+        expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+      },
+    }),
+  }),],
+  providers: [AuthenticationService, LocalStrategy, JwtStrategy],
+  controllers: [AuthenticationController]
+})
+export class AuthenticationModule { }
